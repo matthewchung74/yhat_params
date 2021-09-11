13ba987d681091087ad512583eaea0faf7960c12
Matt C <matt@yhat.pub>
2021-09-10 22:49:02 -0700

    Fixing setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='inference_params',
-    version='0.0.1',
-    description='',
-    url='https://github.com/matthewchung74/inference_params',
-    author='Matthew Chung',
-    author_email='info@mclabs.me',
-    license='unlicense',
-    packages=['inference_params'],
-    zip_safe=False
+    name="inference_params",
+    version="0.0.1",
+    description="",
+    url="https://github.com/yhatpub/yhat_params",
+    author="Matthew Chung",
+    author_email="matt@yhat.pub",
+    license="unlicense",
+    packages=["yhat_params"],
+    zip_safe=False,
 )
